8733d3d1dab6966745da503c23572613cc6591b2
Truong Duong <duongtd@sololab.net>
2021-01-30 17:06:30 +0700

    implement inertiajs with vue and ziggy router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,22 @@
 require('./bootstrap');
 
 require('alpinejs');
+
+// Import modules...
+import Vue from 'vue';
+import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue';
+
+Vue.mixin({ methods: { route } });
+Vue.use(InertiaPlugin);
+
+const app = document.getElementById('app');
+
+new Vue({
+    render: (h) =>
+        h(InertiaApp, {
+            props: {
+                initialPage: JSON.parse(app.dataset.page),
+                resolveComponent: (name) => require(`./Pages/${name}`).default,
+            },
+        }),
+}).$mount(app);
