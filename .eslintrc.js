5ad87e008d1094df1b05279f28a6cd361ad5c3cd
Truong Duong <duongtd@sololab.net>
2021-02-02 07:23:17 +0700

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    env: {
+        node: true,
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:vue/recommended",
+    ],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: "module",
+    },
+    plugins: ["vue"],
+    rules: {
+        "vue/html-indent": ["error", 4, {
+            attribute: 1,
+            baseIndent: 1,
+            closeBracket: 0,
+            alignAttributesVertically: true,
+        }],
+        "vue/script-indent": ["error", 4, {baseIndent: 1, switchCase: 1}],
+        "vue/require-prop-types": 0,
+        "semi": "error",
+        "comma-dangle": ["error", "always-multiline"],
+    },
+    overrides: [
+        {
+            files: ["*.vue"],
+            rules: {
+                indent: "off",
+            },
+        },
+    ],
+    globals: {
+        "axios": true,
+    },
+};
