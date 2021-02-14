3ca49ad7f0554e73abdbc2de8d4311ccca379032
Truong Duong <duongtd@sololab.net>
2021-02-14 14:45:57 +0700

    use narrow symbol for currency display in filter

--- resources/js/Filters/currency.js
+++ resources/js/Filters/currency.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 
 Vue.filter("currency", (value, currency = 'VND') => {
     return (new Intl.NumberFormat('vi-VN', {
+        currencyDisplay: 'narrowSymbol',
         style: 'currency',
         currency: currency,
     })).format(value);
