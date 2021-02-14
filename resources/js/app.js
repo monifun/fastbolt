c0a2b3370b3a1fb6ded3c7cee33f41900c1d65b8
Truong Duong <duongtd@sololab.net>
2021-02-14 15:57:41 +0700

    apply global filter for date

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,16 @@ require('./bootstrap');
 
 require('alpinejs');
 
+require('@/Filters/currency');
+
 // Import modules...
 import Vue from 'vue';
 import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue';
+import VueDateFns from "vue-date-fns";
 
 Vue.mixin({ methods: { route } });
 Vue.use(InertiaPlugin);
+Vue.use(VueDateFns, "HH:mm:ss dd-MM-yyyy");
 
 const app = document.getElementById('app');
 
