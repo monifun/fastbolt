44454911cc77e2f1a411ae10716f6fb1c1446274
Truong Duong <duongtd@sololab.net>
2021-02-18 15:43:07 +0700

    use gate to check for admin access

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('viewAdmin', function ($user) {
+            return in_array($user->email, config('fastbolt.admin_emails'));
+        });
     }
 }
