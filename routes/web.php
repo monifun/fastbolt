4e317ac5f855765e7080251b5a49940e06c75f5e
Truong Duong <duongtd@sololab.net>
2021-02-18 07:50:55 +0700

    cleanup web routes

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::group(['middleware' => 'auth', 'prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::get('dashboard', \App\Http\Controllers\Admin\DashboardController::class)->name('dashboard');
+    Route::resource('users', \App\Http\Controllers\Admin\UserController::class);
+    Route::resource('markets', \App\Http\Controllers\Admin\MarketController::class);
+    Route::resource('vendors', \App\Http\Controllers\Admin\VendorController::class);
+    Route::resource('orders', \App\Http\Controllers\Admin\OrderController::class);
+    Route::resource('products', \App\Http\Controllers\Admin\ProductController::class);
+    Route::resource('products.charges', \App\Http\Controllers\Admin\ProductChargeController::class);
+});
+
+Route::group(['middleware' => 'auth', 'prefix' => 'user', 'as' => 'user.'], function () {
+    Route::get('dashboard', \App\Http\Controllers\User\DashboardController::class)->name('dashboard');
+});
 
 require __DIR__.'/auth.php';
