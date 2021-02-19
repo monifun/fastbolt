89912f484c61e4f0e0e1045296a9962598ce1101
Truong Duong <duongtd@sololab.net>
2021-02-19 14:57:49 +0700

    User model should implement verify class

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Propaganistas\LaravelPhone\Exceptions\CountryCodeException;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
 
