549f008db9d36d55130cedb40b628d416302a6ed
Truong Duong <duongtd@sololab.net>
2021-02-19 14:58:18 +0700

    implement user blank dashboard

---
+++ resources/js/Pages/User/Dashboard.vue
@@ -0,0 +1,31 @@
+<template>
+    <user-layout>
+        <template #header>
+            <h1 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h1>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        You're logged in!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </user-layout>
+</template>
+
+<script>
+    import UserLayout from "@/Layouts/UserLayout";
+    export default {
+        name: "Dashboard",
+        components: {UserLayout},
+    };
+</script>
+
+<style scoped>
+
+</style>
