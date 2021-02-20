b4d0917a86883b1f81d72835a4a947843abe5626
Truong Duong <duongtd@sololab.net>
2021-02-20 16:39:29 +0700

    rework currency filter as method

--- resources/js/Filters/currency.js
+++ resources/js/Filters/currency.js
@@ -1,9 +1,7 @@
-import Vue from "vue";
-
-Vue.filter("currency", (value, currency = 'VND') => {
+export default function currencyFilter(value, currency = 'VND') {
     return (new Intl.NumberFormat('vi-VN', {
         currencyDisplay: 'narrowSymbol',
         style: 'currency',
         currency: currency,
     })).format(value);
-});
+}
