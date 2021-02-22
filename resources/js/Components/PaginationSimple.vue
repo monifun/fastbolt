6adddc1672c5f7228250b9aa9f0f56184e5b1833
Truong Duong <duongtd@sololab.net>
2021-02-22 16:00:48 +0700

    add simple pagination vue component

---
+++ resources/js/Components/PaginationSimple.vue
@@ -0,0 +1,53 @@
+<template>
+    <nav
+        class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6"
+        aria-label="Pagination"
+    >
+        <div class="hidden sm:block">
+            <p class="text-sm text-gray-700">
+                Showing
+                <span class="font-medium">{{ from }}</span>
+                to
+                <span class="font-medium">{{ to }}</span>
+                of
+                <span class="font-medium">{{ total }}</span>
+                results
+            </p>
+        </div>
+        <div class="flex-1 flex justify-between sm:justify-end">
+            <inertia-link
+                v-if="prevPageUrl !== null"
+                :href="prevPageUrl"
+                class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            >
+                Previous
+            </inertia-link>
+            <span
+                v-else
+                class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-500 cursor-default"
+            >
+                Previous
+            </span>
+            <inertia-link
+                v-if="nextPageUrl !== null"
+                :href="nextPageUrl"
+                class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            >
+                Next
+            </inertia-link>
+            <span
+                v-else
+                class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-500 cursor-default"
+            >
+                Next
+            </span>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "PaginationSimple",
+        props: ['from', 'to', 'total', 'prevPageUrl', 'nextPageUrl'],
+    };
+</script>
