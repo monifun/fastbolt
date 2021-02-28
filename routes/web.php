8d4d674b4642387a563332f9126a5ccebb2353a9
Truong Duong <duongtd@sololab.net>
2021-02-28 08:11:12 +0700

    new routes

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,14 @@ Route::get('/', function () {
     ]);
 });
 
-Route::group(['middleware' => ['auth', 'can:viewAdmin'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
+Route::group(['middleware' => ['auth', 'can:viewAdmin'], 'prefix' => config('fastbolt.admin_prefix'), 'as' => 'admin.'], function () {
     Route::get('dashboard', \App\Http\Controllers\Admin\DashboardController::class)->name('dashboard');
     Route::resource('users', \App\Http\Controllers\Admin\UserController::class);
     Route::resource('markets', \App\Http\Controllers\Admin\MarketController::class);
     Route::resource('vendors', \App\Http\Controllers\Admin\VendorController::class);
     Route::resource('orders', \App\Http\Controllers\Admin\OrderController::class);
+    Route::resource('orders.charges', \App\Http\Controllers\Admin\OrderChargeController::class);
+    Route::resource('orders.transactions', \App\Http\Controllers\Admin\OrderTransactionController::class);
     Route::resource('products', \App\Http\Controllers\Admin\ProductController::class);
     Route::resource('products.charges', \App\Http\Controllers\Admin\ProductChargeController::class);
 });
@@ -37,6 +39,10 @@ Route::group(['middleware' => ['auth', 'verified'], 'prefix' => 'user', 'as' => 
     Route::get('profile', [\App\Http\Controllers\User\ProfileController::class, 'show'])->name('profile.show');
     Route::put('profile', [\App\Http\Controllers\User\ProfileController::class, 'update'])->name('profile.update');
     Route::put('password', \App\Http\Controllers\User\PasswordController::class)->name('password.update');
+    Route::get('wallet', [\App\Http\Controllers\User\WalletController::class, 'show'])->name('wallet.show');
+    Route::get('orders', [\App\Http\Controllers\User\OrderController::class, 'index'])->name('orders.index');
+    Route::get('orders/{order}', [\App\Http\Controllers\User\OrderController::class, 'show'])->name('orders.show');
+    Route::post('orders/{order}/payments', [\App\Http\Controllers\User\OrderPaymentController::class, 'store'])->name('orders.payments.store');
 });
 
 require __DIR__.'/auth.php';
