149f6a5310c6f58bfca649706b8266f56ca86ed4
Truong Duong <duongtd@sololab.net>
2021-03-02 09:31:46 +0700

    add enum locale for english language

--- resources/lang/en/enums.php
+++ resources/lang/en/enums.php
@@ -5,6 +5,11 @@ return [
         \App\Enums\OrderStatus::PENDING => 'Pending',
         \App\Enums\OrderStatus::PROCESSING => 'Processing',
         \App\Enums\OrderStatus::COMPLETED => 'Completed',
-        \App\Enums\OrderStatus::CANCELED => 'Canceled'
-    ]
+        \App\Enums\OrderStatus::CANCELED => 'Canceled',
+    ],
+    \App\Enums\ChargeType::class => [
+        \App\Enums\ChargeType::FEE => 'Fee',
+        \App\Enums\ChargeType::TAX => 'Tax',
+        \App\Enums\ChargeType::OTHER => 'Other',
+    ],
 ];
