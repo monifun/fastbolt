0d1e84f3060ab26a1d6d760a4adf3333246c74a7
Truong Duong <duongtd@sololab.net>
2021-03-04 09:00:58 +0700

    check if card has title

--- resources/js/Components/Card.vue
+++ resources/js/Components/Card.vue
@@ -1,10 +1,16 @@
 <template>
     <div class="bg-white shadow sm:rounded-lg overflow-hidden">
         <!-- Title and Description -->
-        <div class="px-4 py-5 border-b border-gray-200 sm:px-6">
+        <div
+            v-if="hasTitle || hasDescription"
+            class="px-4 py-5 border-b border-gray-200 sm:px-6"
+        >
             <div class="-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-nowrap">
                 <div class="ml-4 mt-2">
-                    <h3 class="text-lg leading-6 font-medium text-gray-900">
+                    <h3
+                        v-if="hasTitle"
+                        class="text-lg leading-6 font-medium text-gray-900"
+                    >
                         <slot name="title" />
                     </h3>
                     <p
@@ -42,6 +48,9 @@
     export default {
         name: 'Card',
         computed: {
+            hasTitle() {
+                return !! this.$slots.title;
+            },
             hasDescription() {
                 return !! this.$slots.description;
             },
