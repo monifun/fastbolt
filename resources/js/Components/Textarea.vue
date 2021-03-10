bb9fa7739a3b971ae864ed9a0a36719216c60f87
Truong Duong <duongtd@sololab.net>
2021-03-10 15:14:19 +0700

    add textarea component

---
+++ resources/js/Components/Textarea.vue
@@ -0,0 +1,20 @@
+<template>
+    <textarea
+        ref="textarea"
+        class="rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+    />
+</template>
+
+<script>
+    export default {
+        props: ['modelValue'],
+        emits: ['update:modelValue'],
+        methods: {
+            focus() {
+                this.$refs.textarea.focus();
+            },
+        },
+    };
+</script>
