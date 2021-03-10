fc8d91db619ae8998ee132b4051ef3f510a298f5
Truong Duong <duongtd@sololab.net>
2021-03-10 09:45:27 +0700

    use morph map for polymorphic relation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Relation::morphMap([
+            'user' => 'App\Models\User',
+            'order' => 'App\Models\Order',
+            'product' => 'App\Models\Products',
+        ]);
     }
 }
