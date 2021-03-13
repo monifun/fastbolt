cebe25eb8d683fdf0a6eedd5cf44aa81e4567446
Truong Duong <duongtd@sololab.net>
2021-03-13 15:24:32 +0700

    user vietnamese for user recent orders in dashboard

--- resources/js/Pages/User/Dashboard.vue
+++ resources/js/Pages/User/Dashboard.vue
@@ -252,19 +252,19 @@
                                             scope="col"
                                             class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap"
                                         >
-                                            Products
+                                            Sản phẩm
                                         </th>
                                         <th
                                             scope="col"
                                             class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap"
                                         >
-                                            Status
+                                            Trạng thái
                                         </th>
                                         <th
                                             scope="col"
                                             class="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap"
                                         >
-                                            Created
+                                            Ngày tạo
                                         </th>
                                     </tr>
                                 </thead>
