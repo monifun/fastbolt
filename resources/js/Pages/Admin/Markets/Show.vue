a7fd5c4c852041b1081589bf4f0567c0f5c17ede
Truong Duong <duongtd@sololab.net>
2021-03-17 11:44:11 +0700

    improve under construction view

--- resources/js/Pages/Admin/Markets/Show.vue
+++ resources/js/Pages/Admin/Markets/Show.vue
@@ -32,10 +32,20 @@
         </template>
 
         <div class="py-12">
-            <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
-                <p class="text-center">
-                    Nội dung trang đang được hoàn thiện, vui lòng trở lại sau.
-                </p>
+            <div class="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="flex items-center justify-center">
+                    <inline-svg
+                        src="/svg/building_websites.svg"
+                        fill="none"
+                        aria-label="đang xây dựng"
+                        class="h-60 w-auto"
+                    />
+                </div>
+                <div class="mt-3 text-center sm:mt-5">
+                    <p class="text-gray-500">
+                        Nội dung trang đang được hoàn thiện, vui lòng trở lại sau.
+                    </p>
+                </div>
             </div>
         </div>
     </admin-layout>
