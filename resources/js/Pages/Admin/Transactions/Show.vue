3e877139fb0b2c6375cf2066e06f6ba7e2c80f9c
Truong Duong <duongtd@sololab.net>
2021-03-17 11:30:29 +0700

    add under construction notice

--- resources/js/Pages/Admin/Transactions/Show.vue
+++ resources/js/Pages/Admin/Transactions/Show.vue
@@ -9,6 +9,14 @@
                 </div>
             </div>
         </template>
+
+        <div class="py-12">
+            <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
+                <p class="text-center">
+                    Nội dung trang đang được hoàn thiện, vui lòng trở lại sau.
+                </p>
+            </div>
+        </div>
     </admin-layout>
 </template>
 
